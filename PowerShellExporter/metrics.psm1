b3858416e3147088b421ba85db042d5c387291fc
Enock <38904828+enockkim@users.noreply.github.com>
2023-07-25 10:50:53 +0300

    added local_port stat (#7)
    * added local_address
    * added local_port
    ---------
    
    Co-authored-by: Enock Kimutai <92427438+enock-kim@users.noreply.github.com>

--- PowerShellExporter/metrics.psm1
+++ PowerShellExporter/metrics.psm1
@@ -2,9 +2,11 @@ function Get-TcpConnectionsMetrics {
     Get-NetTCPConnection `
         | Where-Object {$_.RemotePort -ne 0} `
         | Where-Object {$_.LocalAddress -ne '127.0.0.1' -and $_.LocalAddress -ne '::1'} `
-        | Group-Object -Property 'RemoteAddress','RemotePort','State' `
+        | Group-Object -Property 'LocalAddress','LocalPort','RemoteAddress','RemotePort','State' `
         | ForEach-Object {
             [PowerShellExporter.Metric]::new($_.Count, @{
+                'local_address' = $_.Group[0].LocalAddress
+                'local_port' = $_.Group[0].LocalPort
                 'remote_address' = $_.Group[0].RemoteAddress
                 'remote_port' = $_.Group[0].RemotePort
                 'state' = $_.Group[0].State
